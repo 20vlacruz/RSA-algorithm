7137f36f64eb2875b26fb045afe62cb1a58a60ef
Victor Lacruz <20vlacruz@institutjoanbrossa.cat>
2025-09-08 13:44:29 +0200

    v.2.0.2

--- rsa.py
+++ rsa.py
@@ -2,25 +2,22 @@ import random
 import math
 import tkinter
 import webbrowser
+import pyperclip
 
+#Funcions "internes"
 def prime_generator():
     while True:
         number = random.randint(100, 999)
         prime = True
-
         for i in range(2, number):
             if number % i == 0:
                 prime = False
-                break 
-
+                break
         if prime:
             return number
 
-
 def Euler_function(p, q):
-    result = (p-1) * (q - 1)
-    return result
-
+    return (p - 1) * (q - 1)
 
 def find_e(Euler):
     while True:
@@ -28,217 +25,168 @@ def find_e(Euler):
         if math.gcd(e, Euler) == 1:
             return e
 
-def n(p, q):
-    n = p * q
-    return n
-
-
 def cypher(plaintext, e, n):
     plist = []
     for m in plaintext:
         m = ord(m)
-        plist.append((m**e)%n)
-    return(plist)
+        plist.append((m ** e) % n)
+    return plist
 
-def decipher(cyphertxt, n):
+def decipher(cyphertxt, n, d):
     elist = []
     for C in cyphertxt:
         M = pow(C, d, n)
         elist.append(chr(M))
-    result = "".join(elist)
-    return result
+    return "".join(elist)
 
 def opManual():
-    url = "https://docs.google.com/document/d/1jfODmPfew1bb6YUtB_-nZCqmj27m_gtPon8m0G22sNg/edit?pli=1&tab=t.0#heading=h.io896uf4pm76"
-    webbrowser.open(url)
-
-def opRSA():
-    url = "https://docs.google.com/document/d/1jfODmPfew1bb6YUtB_-nZCqmj27m_gtPon8m0G22sNg/edit?pli=1&tab=t.0#heading=h.dhw8vuos626h"
-    webbrowser.open(url)
+    webbrowser.open("https://docs.google.com/document/d/1jfODmPfew1bb6YUtB_-nZCqmj27m_gtPon8m0G22sNg/edit?pli=1&tab=t.0#heading=h.io896uf4pm76")
 
 def opASCII():
-    url = "https://elcodigoascii.com.ar/"
-    webbrowser.open(url)
+    webbrowser.open("https://elcodigoascii.com.ar/")
 
+def opTeoria():
+    webbrowser.open("https://docs.google.com/document/d/1jfODmPfew1bb6YUtB_-nZCqmj27m_gtPon8m0G22sNg/edit?pli=1&tab=t.0#heading=h.dhw8vuos626h")
 
-p = prime_generator()
-q = prime_generator()
-Euler = Euler_function(p, q)
-e = find_e(Euler)
-n = n(p,q)
-d = pow(e, -1, Euler)
+#Variables globals
+p = q = n = e = d = Euler = None
+text = ""
+C = []
+deciphertxt = ""
 
-# Configuració principal
+#Finestra
 window = tkinter.Tk()
-window.geometry("800x600")
+window.geometry("950x650")
 window.resizable(False, False)
 window.title("RSA - Víctor Lacruz")
 window.config(bg="#1f2327")
 icon = tkinter.PhotoImage(file="icon.png")
 window.iconphoto(True, icon)
 
+label_style = {"bg": "#1f2327", "fg": "#b6b6b7", "font": ("Arial", 12)}
 
-# Entrada principal
-title = tkinter.Label(text="Encriptador RSA",font=("Arial", 20),width=40,bg="#1f2327",fg="#b6b6b7",relief="flat")
-title.grid(row=0, column=0, padx=10, pady=15, columnspan=3, sticky="nsew")
+entry_style = {"bg": "#181c20", "fg": "#b6b6b7", "relief": "flat", "insertbackground": "white", "font": ("Arial", 13), "justify": "center"}
 
-entrada = tkinter.Entry(
-    window, 
-    font=("Arial", 20),
-    width=40, 
-    bg="#181c20", 
-    fg="#b6b6b7", 
-    relief="flat", 
-    insertbackground="white"
-)
-entrada.insert(0, "Introdueix el text a xifrar")
-entrada.grid(row=1, column=0, padx=10, pady=15, columnspan=2, sticky="nsew")
+btn_style = {"bg": "#24282c", "fg": "#e0e0e0", "activebackground": "#3a3f44",
+             "activeforeground": "white", "relief": "flat",
+             "font": ("Arial", 11, "bold"), "width": 18, "height": 2}
 
 
-btn_delete = tkinter.Button(
-    window,
-    text="Esborrar",
-    font=("Arial", 12),
-    bg="#24282c",
-    fg="#e0e0e0",
-    relief="flat",
-    activebackground="#3a3f44",
-    activeforeground="white",
-    width=15,
-    height=2,
-    command=lambda:delete()
-)
-btn_delete.grid(row=1, column=2, padx=10, pady=15, sticky="nsew")
+titulo = tkinter.Label(window, text="Xifrar amb RSA", font=("Arial", 22, "bold"), bg="#1f2327", fg="#e0e0e0")
+titulo.pack(pady=20)
 
 
-def on_entry_click(event):
-    if entrada.get() != "":
-        entrada.delete(0, "end")
-    entrada.config(fg="#e0e0e0")
+frame_keys = tkinter.Frame(window, bg="#1f2327")
+frame_keys.pack(pady=10)
 
-def on_focusout(event):
-    if entrada.get() == "":
-        entrada.insert(0, "Introdueix el text a xifrar")
-        entrada.config(fg="#808285") 
+lbl_p = tkinter.Label(frame_keys, text="p", **label_style); lbl_p.grid(row=0, column=0, padx=10, pady=5)
+ent_p = tkinter.Entry(frame_keys, width=15, **entry_style); ent_p.grid(row=1, column=0, padx=10, pady=5)
 
-def focusout(event):
-    widget = event.widget
-    if not isinstance(widget, tkinter.Entry):
-        window.focus()
+lbl_q = tkinter.Label(frame_keys, text="q", **label_style); lbl_q.grid(row=0, column=1, padx=10, pady=5)
+ent_q = tkinter.Entry(frame_keys, width=15, **entry_style); ent_q.grid(row=1, column=1, padx=10, pady=5)
 
-window.bind_all("<Button-1>", focusout)
-entrada.bind("<FocusIn>", on_entry_click)
-entrada.bind("<FocusOut>", on_focusout)
+lbl_n = tkinter.Label(frame_keys, text="n = p·q", **label_style); lbl_n.grid(row=0, column=2, padx=10, pady=5)
+ent_n = tkinter.Entry(frame_keys, width=15, **entry_style); ent_n.grid(row=1, column=2, padx=10, pady=5)
 
-# Títuls columnes
-lbl0 = tkinter.Label(text="Opcions per Xifrar", font=("Arial", 15, "bold"), bg="#1f2327", fg="#b6b6b7", pady=10)
-lbl0.grid(row=2, column=0, padx=20, pady=10)
-lbl1 = tkinter.Label(text="Opcions per Desxifrar", font=("Arial", 15, "bold"), bg="#1f2327", fg="#b6b6b7", pady=10)
-lbl1.grid(row=2, column=1, padx=20, pady=10)
-lbl2 = tkinter.Label(text="Recursos", font=("Arial", 15, "bold"), bg="#1f2327", fg="#b6b6b7", pady=10)
-lbl2.grid(row=2, column=2, padx=20, pady=10)
+lbl_phi = tkinter.Label(frame_keys, text="φ(n)", **label_style); lbl_phi.grid(row=2, column=0, padx=10, pady=5)
+ent_phi = tkinter.Entry(frame_keys, width=15, **entry_style); ent_phi.grid(row=3, column=0, padx=10, pady=5)
 
-# Estil dels botons
-btn_style = {"bg": "#24282c", "fg": "#e0e0e0", "activebackground": "#3a3f44", "activeforeground": "white","relief": "flat", 
-             "font": ("Arial", 11), "width": 22, "height": 2}
+lbl_e = tkinter.Label(frame_keys, text="e (clau pública)", **label_style); lbl_e.grid(row=2, column=1, padx=10, pady=5)
+ent_e = tkinter.Entry(frame_keys, width=15, **entry_style); ent_e.grid(row=3, column=1, padx=10, pady=5)
 
-# Columna 0 - Xifrar
-b0_0 = tkinter.Button(window, text="Guardar text", **btn_style, command= lambda: savetxt())
-b0_0.grid(row=3, column=0, pady=5, padx=15)
-b0_1 = tkinter.Button(window, text="Mostrar p i q", **btn_style, command=lambda:showprimes(p, q))
-b0_1.grid(row=4, column=0, pady=5, padx=15)
-b0_2 = tkinter.Button(window, text="Mostrar n", **btn_style, command=lambda:shown(n))
-b0_2.grid(row=5, column=0, pady=5, padx=15)
-b0_3 = tkinter.Button(window, text="Mostrar φ", **btn_style, command=lambda:showphi(Euler))
-b0_3.grid(row=6, column=0, pady=5, padx=15)
-b0_4 = tkinter.Button(window, text="Xifrar", **btn_style, command=lambda: savecypher(text, e, n))
-b0_4.grid(row=7, column=0, pady=5, padx=15)
-b0_5 = tkinter.Button(window, text="Mostrar Clau Pública", **btn_style, command=lambda:showpublic())
-b0_5.grid(row=8, column=0, pady=5, padx=15)
+lbl_d = tkinter.Label(frame_keys, text="d (clau privada)", **label_style); lbl_d.grid(row=2, column=2, padx=10, pady=5)
+ent_d = tkinter.Entry(frame_keys, width=15, **entry_style); ent_d.grid(row=3, column=2, padx=10, pady=5)
 
-# Columna 1 - Desxifrar
-b1_0 = tkinter.Button(window, text="Mostrar text xifrat", **btn_style, command=lambda: showcypher(C))
-b1_0.grid(row=3, column=1, pady=5, padx=15)
-b1_1 = tkinter.Button(window, text="Mostrar d", **btn_style, command= lambda: showd())
-b1_1.grid(row=4, column=1, pady=5, padx=15)
-b1_2 = tkinter.Button(window, text="Desxifrar", **btn_style, command= lambda: savedecipher())
-b1_2.grid(row=5, column=1, pady=5, padx=15)
-b1_3 = tkinter.Button(window, text="Mostrar text desxifrat", **btn_style, command=lambda: showdecipher(deciphertxt))
-b1_3.grid(row=6, column=1, pady=5, padx=15)
-b1_3 = tkinter.Button(window, text="Mostrar clau privada", **btn_style, command=lambda:showprivate())
-b1_3.grid(row=7, column=1, pady=5, padx=15)
+#Scroll
+def make_scroll_entry(parent, width=30):
+    frame = tkinter.Frame(parent, bg="#1f2327")
+    entry = tkinter.Entry(frame, width=width, **entry_style)
+    entry.pack(side="top", fill="x", padx=2)
 
-# Columna 2 - Recursos
-b2_0 = tkinter.Button(window, text="Instruccions Aplicació", **btn_style, command=opManual)
-b2_0.grid(row=3, column=2, pady=5, padx=15)
-b2_1 = tkinter.Button(window, text="Manual de teoria RSA", **btn_style)
-b2_1.grid(row=4, column=2, pady=5, padx=15)
-b2_2 = tkinter.Button(window, text="Taula de conversió ASCII", **btn_style, command=opASCII)
-b2_2.grid(row=5, column=2, pady=5, padx=15)
+    scrollbar = tkinter.Scrollbar(frame, orient="horizontal", command=entry.xview, bg="#24282c", troughcolor="#181c20")
+    scrollbar.pack(side="bottom", fill="x")
 
-# Expandir columnes
-window.grid_columnconfigure(0, weight=1)
-window.grid_columnconfigure(1, weight=1)
-window.grid_columnconfigure(2, weight=1)
+    entry.config(xscrollcommand=scrollbar.set)
+    return entry, frame
 
-#Funcions UI
-def delete():
-    entrada.delete(0, "end")
-    entrada.insert(0, "Introdueix el text a xifrar")
+frame_msgs = tkinter.Frame(window, bg="#1f2327")
+frame_msgs.pack(pady=20)
+
+# Missatge original
+lbl_msg = tkinter.Label(frame_msgs, text="Missatge original", **label_style)
+lbl_msg.grid(row=0, column=0, padx=20, pady=5)
+ent_msg, frame_msg = make_scroll_entry(frame_msgs, width=30)
+frame_msg.grid(row=1, column=0, padx=20, pady=5)
+
+# Missatge xifrat
+lbl_cyph = tkinter.Label(frame_msgs, text="Missatge xifrat", **label_style)
+lbl_cyph.grid(row=0, column=1, padx=20, pady=5)
+ent_cyph, frame_cyph = make_scroll_entry(frame_msgs, width=30)
+frame_cyph.grid(row=1, column=1, padx=20, pady=5)
+
+# Missatge desxifrat
+lbl_dec = tkinter.Label(frame_msgs, text="Missatge desxifrat", **label_style)
+lbl_dec.grid(row=0, column=2, padx=20, pady=5)
+ent_dec, frame_dec = make_scroll_entry(frame_msgs, width=30)
+frame_dec.grid(row=1, column=2, padx=20, pady=5)
+
+#Funcions dels Botons
+def generar():
+    global p, q, n, Euler, e, d
+    p = prime_generator()
+    q = prime_generator()
+    Euler = Euler_function(p, q)
+    e = find_e(Euler)
+    n = p * q
+    d = pow(e, -1, Euler)
     
-def showprimes(p, q):
-    entrada.delete(0, "end")
-    entrada.insert(0, f"p = {p} i q = {q}")
+    ent_p.delete(0, "end"); ent_p.insert(0, str(p))
+    ent_q.delete(0, "end"); ent_q.insert(0, str(q))
+    ent_n.delete(0, "end"); ent_n.insert(0, str(n))
+    ent_phi.delete(0, "end"); ent_phi.insert(0, str(Euler))
+    ent_e.delete(0, "end"); ent_e.insert(0, str(e))
+    ent_d.delete(0, "end"); ent_d.insert(0, str(d))
 
-def shown(n):
-    entrada.delete(0, "end")
-    entrada.insert(0, f"n = {n}")
+def xifrar():
+    global text, C
+    text = ent_msg.get()
+    C = cypher(text, e, n)
+    ent_cyph.delete(0, "end")
+    ent_cyph.insert(0, str(C))
 
-def showphi(phi):
-    entrada.delete(0, "end")
-    entrada.insert(0, f"φ = {phi}")
-
-text = ""
-def savetxt():
-    global text
-    text = entrada.get()
-    entrada.delete(0, "end")
-    entrada.insert(0, "El text s'ha guardat correctament.")
-    return text
-
-C=""
-def savecypher(ptext, e, n):
-    global C
-    C = cypher(ptext, e, n)
-    entrada.delete(0, "end")
-    entrada.insert(0, f"El text s'ha xifrat correctament.")
-
-def showcypher(C):
-    entrada.delete(0, "end")
-    entrada.insert(0, C)
-
-def showd():
-    entrada.delete(0, "end")
-    entrada.insert(0, f"d = {d}")
-
-deciphertxt =""
-def savedecipher():
-    global C
-    global n
+def desxifrar():
     global deciphertxt
-    deciphertxt = decipher(C, n)
-    entrada.delete(0, "end")
-    entrada.insert(0, "El text s'ha desxifrat correctament.")
+    deciphertxt = decipher(C, n, d)
+    ent_dec.delete(0, "end")
+    ent_dec.insert(0, deciphertxt)
 
-def showdecipher(D):
-    entrada.delete(0, "end")
-    entrada.insert(0, D)
+def copy():
+    text = ent_cyph.get()
+    pyperclip.copy(text)
 
-def showpublic():
-    entrada.delete(0, "end")
-    entrada.insert(0, f"({e}, {n})")
+# Botons
+frame_btns = tkinter.Frame(window, bg="#1f2327")
+frame_btns.pack(pady=30)
 
-def showprivate():
-    entrada.delete(0, "end")
-    entrada.insert(0, f"({d}, {n})")
+btn_gen = tkinter.Button(frame_btns, text="Generar claus", **btn_style, command=generar)
+btn_gen.grid(row=0, column=0, padx=20, pady=10, ipadx=5, ipady=5)
+
+btn_xif = tkinter.Button(frame_btns, text="Xifrar missatge", **btn_style, command=xifrar)
+btn_xif.grid(row=0, column=1, padx=20, pady=10, ipadx=5, ipady=5)
+
+btn_desxif = tkinter.Button(frame_btns, text="Desxifrar missatge", **btn_style, command=desxifrar)
+btn_desxif.grid(row=0, column=2, padx=20, pady=10, ipadx=5, ipady=5)
+
+btn_manual = tkinter.Button(frame_btns, text="Manual", **btn_style, command=opManual)
+btn_manual.grid(row=1, column=0, padx=20, pady=10, ipadx=5, ipady=5)
+
+btn_teoria = tkinter.Button(frame_btns, text="Teoria RSA", **btn_style, command=opTeoria)
+btn_teoria.grid(row=1, column=1, padx=20, pady=10, ipadx=5, ipady=5)
+
+btn_ascii = tkinter.Button(frame_btns, text="Taula ASCII", **btn_style, command=opASCII)
+btn_ascii.grid(row=1, column=2, padx=20, pady=10, ipadx=5, ipady=5)
+
+btn_copy = tkinter.Button(frame_btns, text="Copiar Xifrat", **btn_style, command=copy)
+btn_copy.grid(row=2, column=1, padx=20, pady=10, ipadx=5, ipady=5)
+
 window.mainloop()
